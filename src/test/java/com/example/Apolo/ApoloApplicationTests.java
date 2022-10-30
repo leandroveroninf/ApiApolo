d506d63b935090c226e41a87d08cb2c206db12a5
leandroVeron <leandrove@epidataconsulting.com>
2022-10-30 16:23:31 -0300

    Initial commit

---
+++ src/test/java/com/example/Apolo/ApoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Apolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
